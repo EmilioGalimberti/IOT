f6ff3fba08ac5690188312145ed3882dcdd8e093
EmilioGalimberti <emiliogalimberty@gmail.com>
2023-08-21 02:21:03 -0300

    primer ejercicio con esp32 pulsador + led

---
+++ esp32 pulsador + led/main.py
@@ -0,0 +1,17 @@
+#https://wokwi.com/projects/370160487629016065
+from machine import Pin
+import time
+
+print("esperando pulsador")
+
+sw = Pin(23, Pin.IN)
+led = Pin(2, Pin.OUT)
+
+contador=0
+while True:
+    if sw.value():
+        led.value(not led.value())
+        contador += 1
+        print(contador)
+        time.sleep_ms(250)
+    #El tiempo de de espera le permite reaccionar bien al prendido y apagado contra el pulsador
